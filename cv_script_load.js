97919f36c635729e5a769aa686eca94b7b8824c4
Beo Hoang <beohoang98@users.noreply.github.com>
2017-12-23 19:08:34 +0700

    sticky info's title

--- cv_script_load.js
+++ cv_script_load.js
@@ -1,18 +1,18 @@
 let oldDocScroll = 0;
-$(window).on("scroll", function(){
-	let docScroll = $(this).scrollTop();
+// $(window).on("scroll", function(){
+// 	let docScroll = $(this).scrollTop();
 
-	$(".info").each((i, val)=>{
-		var nameTop = $(val).offset().top - docScroll;
-		let itname = $(val).find(".info-name");
-		if (nameTop <= 0) {
-			itname.addClass("info-name-fixed");
-		}
-		else {
-			itname.removeClass("info-name-fixed");
-		}
-	});
-});
+// 	$(".info").each((i, val)=>{
+// 		var nameTop = $(val).offset().top - docScroll;
+// 		let itname = $(val).find(".info-name");
+// 		if (nameTop <= 0) {
+// 			itname.addClass("info-name-fixed");
+// 		}
+// 		else {
+// 			itname.removeClass("info-name-fixed");
+// 		}
+// 	});
+// });
 
 
 $(document).ready(function() {
@@ -20,19 +20,4 @@ $(document).ready(function() {
 		let url=$(this).attr("pic");
 		$(this).css("background-image", "url(\""+url+"\")");
 	});
-});
-
-let scrollDistance = 200;
-$(window).on("mousewheel DOMMouseScroll", function(event){
-		event.preventDefault();	
-										
-		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-		var scrollTop = $(window).scrollTop();
-		var finalScroll = -parseInt(delta*scrollDistance);
-		
-		window.scrollBy({
-			top: finalScroll,
-			left: 0,
-			behavior: "smooth"
-		})
 });
