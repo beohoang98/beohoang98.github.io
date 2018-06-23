5886955203ab116633d98c8cf36f0e03eddc279a
Beo Hoang <beohoang98@users.noreply.github.com>
2018-06-23 14:34:47 +0700

    add gh-page with jekyll

---
+++ _posts/2018-6-22-Get-FB-Photo.md
@@ -0,0 +1,26 @@
+---
+layout: post
+title: Tôi đã download ảnh crush trên facebook như thế nào?
+categories: coding
+tags: javascript php hack-stuff
+author: BeoHoang
+img: /images/2018-6-22-Get-FB-Photo.jpg
+---
+
+# LẤY LINK ẢNH TỪ TRANG PROFILE
+
+## Tìm vị trí link của ảnh
+
+## Viết code JS để chạy auto lấy link
+
+## Download về
+
+# DOWNLOAD ẢNH TỪ DANH SÁCH LINK
+
+## Sử dụng ngôn ngữ PHP
+
+## Cách cắt ra từng link một
+
+## Sử dụng thư viện cURL
+
+## Chạy
---
+++ _site/compressHTML.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const fsCss = require('fs');
+
+let dataCom;
+
+fs.readFile("_index.html", 'utf8', (err, data)=>{
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	let reg = /\<link.*compress.*href=\"(.*\.css)\"\>/g;
+	let dataMatch = data.match(reg);
+	dataCom = data;
+
+	for (let i = 0, len = dataMatch.length; i < len; ++i) {
+		let src = dataMatch[i].match(/href=\"(.*\.css)\"/)[1];
+		console.log(src);
+
+		fsCss.readFile(src, 'utf8', function(cssErr, cssData) {
+			if (cssErr) {
+				console.log(cssErr);
+				return;
+			}
+			else {
+				dataCom = dataCom.replace(dataMatch[i], "<style>"+cssData+"</style>");
+				dataCom = dataCom.replace(/[\t\r\n]+/g, "");
+
+				setTimeout(()=>{
+					fs.writeFile("index.html", dataCom, function(err){
+						if (err) console.log(err);
+					});
+				}, 1000);				
+			}
+		});	
+	}
+});
