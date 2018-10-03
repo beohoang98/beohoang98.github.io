839b66756a1c34d94335112ef205a7e96b2532d9
beohoang98 <beohoang98@user.noreply.github.com>
2018-10-03 23:48:08 +0700

    fix slash error

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ const Page = require('./Component/Page');
     });
 
     router.map('/see-blog/:postName', (req, res) => {
-        page.rerender(`/blog/${req.params.postName}`);
+        page.rerender(`blog/${req.params.postName}`);
         res.end();
     });
 
