ee5f0f423f46cddd2ad0e0467e9ae94917d23577
Beo Hoang <beohoang98@users.noreply.github.com>
2020-11-21 23:09:52 +0700

    Updates

---
+++ precache-manifest.3c8c42dcb3d3d48339b20cb584b2bc1b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2d0a47fb2f99d1274b",
+    "url": "/css/about.d1d0b9c9.css"
+  },
+  {
+    "revision": "8b0d8f9443e2ecd503ae",
+    "url": "/css/app.865f8cc1.css"
+  },
+  {
+    "revision": "20fd1afb9fce0e56dec2",
+    "url": "/css/chunk-6fea7c20.b3fdc5fe.css"
+  },
+  {
+    "revision": "b389e358c522d5c28c692e70d1de8a4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f2d0a47fb2f99d1274b",
+    "url": "/js/about.2c597a9e.js"
+  },
+  {
+    "revision": "8b0d8f9443e2ecd503ae",
+    "url": "/js/app.342c158c.js"
+  },
+  {
+    "revision": "20fd1afb9fce0e56dec2",
+    "url": "/js/chunk-6fea7c20.043c11c8.js"
+  },
+  {
+    "revision": "510cf997c60c1759707c",
+    "url": "/js/chunk-vendors.0d55add7.js"
+  },
+  {
+    "revision": "c472b74121f6230a5211bbf7f261655a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
