227fc3383057eb95e76f652328b4a53796f6964b
Beo Hoang <beohoang98@users.noreply.github.com>
2021-02-27 18:29:59 +0700

    fix-deps-and-lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,11 @@ module.exports = {
         "prettier/prettier": "warn",
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "@typescript-eslint/no-unused-vars":
+            process.env.NODE_ENV === "production" ? "off" : "warn",
+        "@typescript-eslint/explicit-module-boundary-types":
+            process.env.NODE_ENV === "production" ? "off" : "warn",
+        "@typescript-eslint/no-explicit-any":
+            process.env.NODE_ENV === "production" ? "off" : "warn",
     },
 };
