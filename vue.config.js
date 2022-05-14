1dc9cf6324d4f7a93d8a26184f716f32d18fb0a3
An Hoang <beohoang98@gmail.com>
2022-05-14 20:37:13 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     lintOnSave: false,
     parallel: false,
+    css: {
+        loaderOptions: {
+            css: {
+                url: false,
+            }
+        }
+    }
 };
